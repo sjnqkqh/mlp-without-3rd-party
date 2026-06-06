6a2857165e0386802680bc0c8f86658521e15aca
jung.jin <sjn3649@gmail.com>
2026-06-06 14:41:13 +0900

    feature: 기울기 최적화 모듈 구현

---
+++ gradient_descent_optimizer.py
@@ -0,0 +1,14 @@
+class GradientDescentOptimizer:
+    def __init__(self, layer_list, lr=0.1):
+        self.layer_list = layer_list
+        self.lr = lr
+
+    def step(self):
+        for layer in self.layer_list:
+            for i in range(layer.out_features):
+                for j in range(layer.in_features):
+                    # 가중치 조절
+                    layer.weight[i][j] -= self.lr * layer.d_weight[j]
+
+            for i in range(layer.out_features):
+                layer.bias[i] -= self.lr * layer.d_bias[i]
