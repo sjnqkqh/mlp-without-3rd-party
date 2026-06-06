eb869be26d29854d3e710f992d030efd12c062f8
jung.jin <sjn3649@gmail.com>
2026-06-06 13:18:13 +0900

    feature: ReLu 레이어에 Backward 기능 추가 - 손실 전달용 함수 추가 - 매개변수, 필드명 수정 - x라는 네이밍이 값의 성질을 설명하지 못하여 의미상 적합한 단어로 교체

--- relu.py
+++ relu.py
@@ -1,8 +1,18 @@
 class ReLuActivation:
     def __init__(self):
-        self.x = None # 기존에 들어온 값 저장, 역전파 시 미분값 계산을 위해 사용함
+        self.forward_value = None # 기존에 들어온 값 저장, 역전파 시 미분값 계산을 위해 사용함
 
-    def forward(self, x: list):
-        self.x = x
-        outputs = [max(0.0, val) for val in x]
+    def forward(self, forward_value: list):
+        self.forward_value = forward_value
+        outputs = [max(0.0, val) for val in forward_value]
         return outputs
+
+    def backward(self, backward_value: list):
+        next_backward_value = []
+        for i in range(len(self.forward_value)):
+            if self.forward_value[i] > 0:
+                next_backward_value.append(backward_value[i])
+            else:
+                next_backward_value.append(0.0)
+
+        return next_backward_value
